53e1538fb30ed038a034572ca7ee50eaa3426e2d
alelievr <alelievr@student.42.fr>
2018-02-24 13:09:59 +0100

    Added timemap and player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float		speed = 1;
+	public PlayerHead	head;
+
+	[HideInInspector]
+	public bool			dead;
+
+	new Rigidbody2D	rigidbody;
+
+	void Start ()
+	{
+		rigidbody = GetComponent< Rigidbody2D >();
+	}
+	
+	void Update ()
+	{
+		if (head.hit && !dead)
+		{
+			dead = true;
+			Debug.Log("DEAD !");
+		}
+	}
+
+	void FixedUpdate()
+	{
+		Vector2 v = rigidbody.velocity;
+
+		v.x = speed;
+
+		rigidbody.velocity = v;
+	}
+}
