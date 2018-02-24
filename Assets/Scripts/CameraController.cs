839cbc4a9173026ce26f5d94cb67abe0d7bb5cf6
alelievr <alelievr@student.42.fr>
2018-02-24 13:56:37 +0100

    Added cinemachine cameras

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject	playerCamera;
+	public GameObject	previewCamera;
+
+	void Start ()
+	{
+		playerCamera.SetActive(false);
+	}
+	
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			playerCamera.SetActive(true);
+			previewCamera.SetActive(false);
+		}
+	}
+}
